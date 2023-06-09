2d15fd7ba0958c750045059f0e05cf5260dbbd70
Ridley05 <117008284+Ridley05@users.noreply.github.com>
2023-06-09 16:49:08 +1000

    Duplicate files. Reuploaded Samuel's version!

--- GR21A3-main/IAB207A3/musicevent/music/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-mainbp = Blueprint('main', __name__)
-
-@mainbp.route('/')
-def index():
-    # events = events.query.all()
-    return render_template('index.html')
--- GR21A3-main/README.md
+++
@@ -1,2 +0,0 @@
-# GR21A3
-This is IAB207 Assessment 3 with Group 21 Members: Ridley Visan, Jingyuan Cheng (Samuel), Yuan Heng Ang 
