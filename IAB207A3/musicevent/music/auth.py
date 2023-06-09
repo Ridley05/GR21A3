12de77f32625a9d1119548f3a32e29baccf13dad
YuanHeng23 <110651260+YuanHeng23@users.noreply.github.com>
2023-06-09 23:15:05 +1000

    Update auth.py

--- IAB207A3/musicevent/music/auth.py
+++ IAB207A3/musicevent/music/auth.py
@@ -6,26 +6,7 @@ from .models import User
 from . import db
 
 authbp = Blueprint('auth', __name__)
-
-@authbp.route('/login', methods=['GET','Post'])
-def login():
-    login_form = LoginForm()
-    error = None
-    if(login_form.validate_on_submit() == True):
-        user_name = login_form.user_name.data
-        password = login_form.password.data
-        u1 = User.query.filter_by(name=user_name).first()
-        if u1 is None:
-            error = 'Incorrect username'
-        elif not check_password_hash(u1.password_hash, password):
-            error = 'Incorrect password'
-        if error is None:
-            login_user(u1)
-            return redirect(url_for('main.index'))
-        else:
-            flash(error)
-   return render_template('user.html', form=login_form, heading='Login')
-    
+   
 @authbp.route('/register', methods=['GET','POST'])
 def register():
     register = RegisterForm()
@@ -47,7 +28,26 @@ def register():
         return redirect(url_for('main.index'))
     else:
         return render_template('user.html', form=register, heading='Register')
-        
+    
+@authbp.route('/login', methods=['GET','Post'])
+def login():
+    login_form = LoginForm()
+    error = None
+    if(login_form.validate_on_submit() == True):
+        user_name = login_form.user_name.data
+        password = login_form.password.data
+        u1 = User.query.filter_by(name=user_name).first()
+        if u1 is None:
+            error = 'Incorrect username'
+        elif not check_password_hash(u1.password_hash, password):
+            error = 'Incorrect password'
+        if error is None:
+            login_user(u1)
+            return redirect(url_for('main.index'))
+        else:
+            flash(error)
+   return render_template('user.html', form=login_form, heading='Login')
+
 @authbp.route('/logout')
 def logout():
     logout_user()
